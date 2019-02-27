2466c99bf6213e57b96e7f1935d9bfeffc2f49d0
Joshua Min <minje@usc.edu>
2019-02-26 18:42:26 -0800

    Made each item link to /(recipe or restaurant/:id

--- frontend/src/components/ListManagementPage.js
+++ frontend/src/components/ListManagementPage.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 import styled from "styled-components";
 
+/* Start page styling */
 const HeadingLayout = styled.div`
   display: flex;
   flex-direction: row;
@@ -43,22 +44,27 @@ const RecipeItemLayout = styled.div`
   display: flex;
   flex-direction: column;
 `;
+/* End page styling */
 
 let ListManagementPage = props => {
+  /* Initialize page state */
   const [loading, setLoading] = useState(true);
   const [selectedList, setSelectedList] = useState("");
 
+  /* Start Testing data */
   const recipes = [
-    { title: "Recipe 1", prepTime: 3, cookTime: 20, price: 4.15 },
-    { title: "Recipe 2", prepTime: 4, cookTime: 22, price: 8.3 },
-    { title: "Recipe 3", prepTime: 5, cookTime: 24, price: 12.45 }
+    { id: 1, title: "Recipe 1", prepTime: 3, cookTime: 20, price: 4.15 },
+    { id: 2, title: "Recipe 2", prepTime: 4, cookTime: 22, price: 8.3 },
+    { id: 3, title: "Recipe 3", prepTime: 5, cookTime: 24, price: 12.45 }
   ];
   const restaurants = [
-    { title: "Restaurant 1", driveTime: 20, price: 11.95 },
-    { title: "Restaurant 2", driveTime: 5, price: 5.95 },
-    { title: "Restaurant 3", driveTime: 30, price: 5.65 }
+    { id: 4, title: "Restaurant 1", driveTime: 20, price: 11.95 },
+    { id: 5, title: "Restaurant 2", driveTime: 5, price: 5.95 },
+    { id: 6, title: "Restaurant 3", driveTime: 30, price: 5.65 }
   ];
+  /* End Testing Data */
 
+  /* Load data from localStorage */
   useEffect(() => {
     setLoading(false);
   }, []);
@@ -102,26 +108,30 @@ let ListManagementPage = props => {
       </HeadingLayout>
       {recipes.map((recipe, idx) => {
         return (
-          <ItemLayout key={"recipe" + idx} dark={idx % 2 === 0}>
-            <RecipeItemLayout>
-              <h2>{recipe.title}</h2>
-              <p>Cook Time: {recipe.cookTime}</p>
-              <p>Prep Time: {recipe.prepTime}</p>
-            </RecipeItemLayout>
-            <h2>${recipe.price}</h2>
-          </ItemLayout>
+          <Link to={`/recipe/${recipe.id}`} key={"recipe" + idx}>
+            <ItemLayout dark={idx % 2 === 0}>
+              <RecipeItemLayout>
+                <h2>{recipe.title}</h2>
+                <p>Cook Time: {recipe.cookTime}</p>
+                <p>Prep Time: {recipe.prepTime}</p>
+              </RecipeItemLayout>
+              <h2>${recipe.price}</h2>
+            </ItemLayout>
+          </Link>
         );
       })}
       {restaurants.map((restaurant, idx) => {
         let isDark = recipes.length % 2 === 0 ? idx % 2 === 0 : idx % 2 !== 0;
         return (
-          <ItemLayout key={"restaurant" + idx} dark={isDark}>
-            <RestaurantItemLayout>
-              <h2>{restaurant.title}</h2>
-              <p>Drive Time: {restaurant.driveTime} mins</p>
-            </RestaurantItemLayout>
-            <h2>${restaurant.price}</h2>
-          </ItemLayout>
+          <Link to={`/restaurant/${restaurant.id}`} key={"restaurant" + idx}>
+            <ItemLayout dark={isDark}>
+              <RestaurantItemLayout>
+                <h2>{restaurant.title}</h2>
+                <p>Drive Time: {restaurant.driveTime} mins</p>
+              </RestaurantItemLayout>
+              <h2>${restaurant.price}</h2>
+            </ItemLayout>
+          </Link>
         );
       })}
     </div>
