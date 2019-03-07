94fe610706f5302f5228d5d146d14b89b2fe66f5
Josh Min <minje@usc.edu>
2019-03-07 11:04:46 -0800

    HomePage tests work

--- features/step_definitions/HomePage.rb
+++ features/step_definitions/HomePage.rb
@@ -1,13 +1,13 @@
 Given("that I am on the homepage") do
-  visit("http://localhost:3000")
+  visit('http://localhost:3000')
 end
 
 When("I enter {string} into the text box labeled: Enter Food") do |string|
-  find('search').set(string)
+  fill_in 'Enter Food', with: string
 end
 
 When("I enter a number greater than or equal to {int} into the text box with a default value of {int}") do |int, int2|
-  find('numResults').set(int2)
+  fill_in 'numResults', with: int2
 end
 
 When("I click the button with the label: Feed Me!") do
@@ -15,5 +15,5 @@ When("I click the button with the label: Feed Me!") do
 end
 
 Then("I will transition to the Results Page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_current_path('/search')
 end
---
+++ features/step_definitions/SearchPage.rb
@@ -0,0 +1,84 @@
+Given("I am on the Search Page") do
+  visit('http://localhost:3000')
+end
+
+Given("I have entered a search query {string}") do |string|
+  find.('search').set(string)
+end
+
+Given("{int} for the number of results to be displayed") do |int|
+  find.('numResults').set(int)
+end
+
+Given("I clicked the {string} button") do |string|
+  click_on(string)
+end
+
+Given("I am on the Results Page") do
+  expect(page).to have_current_path('/search')
+end
+
+Then("the page will have the title: Results for {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the page will have a collage of photos related to the search query") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the page will have a dropdown box with the predefined lists") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the page will have a button labeled {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the page will have two columns of results titled: Restaurants, Recipes") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the page will have at most {int} restaurant and recipe results in each column") do |int|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("each restaurant item on the page will have an address, name, and minutes") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("each recipe item on the page will have a name, stars, prep time, and cook time") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I select {string} from the dropdown") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click the {string} button") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I will be on the {string} list page") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I will be on the Search Page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click on a restaurant with name {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I will be on the Restaurant Page of restaurant {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click on a recipe with name {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I will be on the Recipe Page of recipe {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
