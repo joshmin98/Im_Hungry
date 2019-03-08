0a5435fca5acd39377ddbab41345aeb3d76281d8
Nhan Tran <trannhan@usc.edu>
2019-03-07 19:58:57 -0800

    fix printable page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import PrintPage from "./components/PrintPage";
 import HomePage from "./components/HomePage";
 import ListManagementPage from "./components/ListManagementPage";
 import RestaurantPage from "./components/RestaurantPage";
@@ -18,6 +18,7 @@ class App extends Component {
             <Route path="/restaurant/:id" component={RestaurantPage} />
             <Route path="/recipe/:id" component={RecipePage} />
             <Route path="/search" component={SearchPage} />
+            <Route path="/print/:name/:id" component={PrintPage} />
           </>
         </Router>
       </div>
